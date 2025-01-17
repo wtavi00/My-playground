1e0d912c917b804450cd4e38b56011d10a7b3a91
wtavi00 <avijittarafder22@gmail.com>
2025-01-17 10:21:02 +0600

    Update Test_Dectionary.py

--- Test_Dectionary.py
+++ Test_Dectionary.py
@@ -210,3 +210,21 @@ num2=3
 num3=5
 print(form_triangle(num1, num2, num3))
 print("\n")
+
+def make_amount(rupees_to_make, no_of_five, no_of_one):
+    # Setting a diffolt valu zero
+    five_needed=0 
+    one_needed=0 
+
+    # Maximize the use of 5 rupee coins
+    five_needed = min(rupees_to_make // 5, no_of_five)
+    remaining_amount = rupees_to_make - (five_needed * 5)
+    
+    # Check if the remaining amount can be covered by 1 rupee coins
+    if remaining_amount <= no_of_one:
+        one_needed = remaining_amount
+        print("No. of Five needed :", five_needed)
+        print("No. of One needed  :", one_needed)
+    else:
+        print(-1)
+        
