80b45f0569003ba64a38335cceae8990e09137ac
Avijit Tarafder <avijittarafder22@gmail.com>
2025-12-01 11:49:23 +0600

    Update Besic_finctions.py

--- Besic_finctions.py
+++ Besic_finctions.py
@@ -7,44 +7,44 @@ another_fn = doc_function
 print(another_fn.__doc__)
 
 def student_details(name, school, math, physics, chemistry, biology, enrolled=False):
+    """student_details("Devid", "Germany", 90, 34, 56, 89, enrolled="Yes")
+    student_details("Jhon", "Irland", 90, 34, 43, 89)"""
+    
     total = math + physics + chemistry + biology
     print("Name:", name)
     print("School:", school, "Enrolled:", enrolled)
     print("Total Score:", total)
 
-student_details("Devid", "Germany", 90, 34, 56, 89, enrolled="Yes")
-student_details("Jhon", "Irland", 90, 34, 43, 89)
-
 def students_in_collage(collage, city,*student):
+    """students_in_collage("Gryffindor", "London","Devid","Melan","Jhon")"""
     print("Collage:",collage)
     print("City:",city)
     print("Student:",student)
-students_in_collage("Gryffindor", "London","Devid","Melan","Jhon")
 
 def student_in_collage(*student,collage="Standford",city="California"):
+    """student_in_collage("Nolen","Elsa","Devid")"""
     print("Collage:",collage)
     print("City:",city)
     print("Student:",student)
-student_in_collage("Nolen","Elsa","Devid")
 
 def students_details(**kwargs):
+    """students_details(Name="Jhon",Age=21)"""
     print(type(kwargs))
     print(kwargs)
-students_details(Name="Jhon",Age=21)
-
+    
 def student_details(**details):
+    """student_details(Name="Devid Melan",Age="21",City="London",University="Gryffindor")"""
     for key, value in details.items():
         print(key,value)
-student_details(Name="Devid Melan",Age="21",City="London",University="Gryffindor")
 
 def student1_details(**details):
+    """student1_details(name="Jhone Cloud", age=21, collage="London")"""
     if 'name' in details:
         print("Name:", details['name'])
     if 'age' in details:
         print("Age:", details['age'])
     if 'collage' in details:
         print("Collage:", details['collage'])
-student1_details(name="Jhone Cloud", age=21, collage="London")
 
 a_list=[43,4,56,8,89321,3,34,52,62,564,673,54,65,56545]
 b_list=[2,3,4,56,7,8,9,1,10,12,13,14]
@@ -57,10 +57,6 @@ def length(some_list):
     print("The length of the list is", count)
     return count
 
-a=length(a_list)
-print(a)
-length(cars_list)
-
 def finde_max_in_list(some_list):
     max_element =  some_list[0]
     length = len(some_list)
@@ -68,15 +64,10 @@ def finde_max_in_list(some_list):
         if some_list[i] > max_element:
             max_element=some_list[i]
     return max_element
-z=finde_max_in_list(a_list)
-print(z)
-
 
 def finde_max(some_list):
     count= max(some_list)
     return count
-x= finde_max(b_list)
-print(x)
 
 empty_list=[]
 def finde_max_in_list(some_list):
@@ -89,12 +80,7 @@ def finde_max_in_list(some_list):
         if some_list[i]>max_element:
             max_element=some_list[i]
     return max_element
-
-c=finde_max_in_list(empty_list)
-print(c)
-d=finde_max_in_list(a_list)
-print(d)
-
+    
 def max_in_empty(some_list):
     if len(some_list)==0:
         print("No element presents")
@@ -102,7 +88,14 @@ def max_in_empty(some_list):
     count=max(some_list)
     print("This is",count)
     return count
-max_in_empty(a_list)
-e=max_in_empty(empty_list)
-f=max_in_empty(a_list)
-print(f)
+
+def finde_first_capital(some_string):
+    capital_letter = None
+    for ch in some_string:
+        if ch.upper() == ch and ch != " ":
+            capital_letter= ch
+            break
+    if capital_letter is None:
+        return "No capital letters found"
+    else:
+        return "First capital letter: " + capital_letter
